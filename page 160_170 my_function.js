6eb6759d991ab872014ab5a901f1e0c6691f2f16
SKindij <88454706+SKindij@users.noreply.github.com>
2022-03-02 18:09:49 +0200

    Update page 160_170 my_function.js
    a function can have several arguments (variables)

--- page 160_170 my_function.js
+++ page 160_170 my_function.js
@@ -6,5 +6,23 @@ var myFirstFunction = function () {
 // add an argument to the function (додаємо аргумент до функції)
 var sayHelloTo = function (Svin) {
 	console.log('Hello ' + Svin + "!");
-}; //then instead of a pig we write what we want to see in the greeting (далі замість свина пишемо шо хочемо побачити в привітанні)
+}; 
+//then instead of a pig we write what we want to see in the greeting (далі замість свина пишемо шо хочемо побачити в привітанні)
+sayHelloTo("Tatko");
+
+var drawCats = function (howManyTimes) { 
+	for (var i = 0; i < howManyTimes; i++) {
+		console.log(i + " =^.^=");
+	}
+};
+//then write how many times we want this function (потім пишемо скільки разів хочемо дану функцію) drawCats(x>0)
+drawCats(5);
+
+//a function can have several arguments (variables) (у функції може бути декілька аргументів(змінних))
+var blabla = function (Svin, Makaka) {
+	console.log('My first argument was: ' + Svin);
+	console.log('My second argument was ' + Makaka);
+}
+
+
 
