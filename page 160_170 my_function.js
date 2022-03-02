86797506af2c740611dfa0a8e6ed13894e3b17df
SKindij <88454706+SKindij@users.noreply.github.com>
2022-03-02 19:39:22 +0200

    Update page 160_170 my_function.js
    areArraysSame?

--- page 160_170 my_function.js
+++ page 160_170 my_function.js
@@ -68,3 +68,32 @@ var medalForScore = function (score) {
 	return 'Golden Pig';
 };
 
+//task # 1
+var add = function (x, y) {
+	return x + y;
+};
+var multiply = function (a, b) {
+	return a * b;
+};
+add(multiply(36325, 9824), 777);
+
+//tak #2
+//I give the function a name (даю функції назву)
+var areArraysSame = function (massive1, massive2) {
+//if the numbers are the same, but there is at least one difference, the computer notices it
+//якщо циферки однакові , але є хоча б одна відмінність, то комп це помічає
+	for (var i = 0; i < massive1.length; i++) {
+    if (massive1[i] !== massive2[i]) {
+      return false;
+    }
+	//I give a condition of performance of a handicap if length (number of digits is not equal to each other)
+	//даю умову виконанння фори, яккщо довжина(кількість циферок не дорівнюж одна одній)
+if (massive1.length !== massive2.length) {
+    return false;
+  }
+	}
+	return true;
+};
+	//and so on we compare what we want (і так далі порівнюємо шо хочемо)
+	areArraysSame([1, 2, 3], [4, 5, 6]);
+        areArraysSame([12, 24, 38], [12, 24, 38]);
