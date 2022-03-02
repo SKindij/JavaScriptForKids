7e942168169cb0bcb1eefb34dac670be03a544b8
SKindij <88454706+SKindij@users.noreply.github.com>
2022-03-02 18:04:34 +0200

    Create page 160_170 my_function.js
    deal with functions

---
+++ page 160_170 my_function.js
@@ -0,0 +1,10 @@
+//deal with functions (розбираємося із функціями)
+var myFirstFunction = function () {
+	console.log('Hello world');
+};
+
+// add an argument to the function (додаємо аргумент до функції)
+var sayHelloTo = function (Svin) {
+	console.log('Hello ' + Svin + "!");
+}; //then instead of a pig we write what we want to see in the greeting (далі замість свина пишемо шо хочемо побачити в привітанні)
+
