706fdbceaa95f15836ad4d52a1e55da94d79b084
SKindij <88454706+SKindij@users.noreply.github.com>
2022-03-05 19:02:09 +0200

    Update example_card file_Netflix.js

--- example_card file_Netflix.js
+++ example_card file_Netflix.js
@@ -1,13 +1,13 @@
 //Netflix movie file (картотека фільмів Нетфлікс)
 var NetflixsMovies = {
 "The Witcher (Відьмак)": {
-releaseDate: 2020,seasons: 1,genre: ["фентезі", "трилер", "історичний"]
+releaseDate: 2020,seasons: 1,genre: ["fantasy", "thriller", "historical"]
 },
 "Altered Carbon (Видозмінений вуглець)": {
-releaseDate: 2019, seasons: 2, genre: ["фантастика", "детектив", "бойовик"]
+releaseDate: 2019, seasons: 2, genre: ["fantasy", "detective", "action"]
 },
 "Lucifer (Люцифер)": {
-releaseDate: 2019, seasons: 5, genre: ["фентезі", "комедія", "детектив"]
+releaseDate: 2019, seasons: 5, genre: ["fantasy", "comedy", "detective"]
 }
 };
 
@@ -17,7 +17,7 @@ Witcher.genre
 
 //when you need to add a new movie (коли треба добавити новий фільм)
 var Discovery = {
-releaseDate: 2019, seasons: 3, genre: ["фантастика", "пригоди", "космос"]
+releaseDate: 2019, seasons: 3, genre: ["fantasy", "adventures", "space"]
 };
 NetflixsMovies["Discovery (Дискавері)"] = Discovery;
 
