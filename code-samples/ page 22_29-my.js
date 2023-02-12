a97ba7443dcefe3d3efe9ae45fab589faeaa0e25
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-12 18:42:55 +0200

    Update and rename page 22_29 my.js to code-samples/ page 22_29-my.js
    calculate the age of the family & combine lines

---
+++ code-samples/ page 22_29-my.js
@@ -0,0 +1,17 @@
+// calculate the age of the family
+let MaxAge = 19;
+let DadAge = 41;
+let MatusyaAge = 39;
+let SvinAge = MaxAge + DadAge + MatusyaAge;
+
+console.log('In February the age of the family is ' + SvinAge);
+
+//and what will happen in April 2023?
+DadAge = 42;
+SvinAge = MaxAge + DadAge + MatusyaAge;
+console.log('In April it will be ' + SvinAge);
+
+//combine lines
+let DadNick = " Svin ";
+let MaxNick = " Boy ";
+DadNick + MaxNick;
