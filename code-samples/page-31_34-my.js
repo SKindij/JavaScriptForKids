428db5e7d0f307f18b9675ba9bcaa4dcc723a0a1
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-12 18:51:36 +0200

    Update and rename page 31_34 my.js to code-samples/page-31_34-my.js
    length of the line **&** secret code 007 **&** edit the text

---
+++ code-samples/page-31_34-my.js
@@ -0,0 +1,31 @@
+//calculate the length of the line
+let x = "Svin";
+let y = "Boy";
+let z = x + y;
+
+console.log(x + ' length = ' + x.length);
+console.log(y + ' length = ' + y.length);
+console.log(z + ' length = ' + z.length);
+
+//secret code 007
+let myName = 'Maxym';
+myName[3]; 
+
+//secret code 007
+let codeWord1 = "super";
+let codeWord2 = "vision";
+let codeWord3 = "is";
+let codeWord4 = 'important';
+codeWord1[0] + codeWord2[0] + codeWord3[0] + codeWord4[7];
+
+//another version;
+let boom = 'my lovely svin';
+boom.slice(7, 11);
+
+//edit the text
+let slovo = 'tAtO MY lOvEly SviN';
+let normslovo = slovo.toLowerCase();
+let bukvaBig = normslovo[0];
+let BigBukva = bukvaBig.toUpperCase();
+let ostatok = normslovo.slice(1);
+BigBukva + ostatok;
