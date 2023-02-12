60bc8addb997bead0f0596f2af989a962e372136
Serhii Kindij <88454706+SKindij@users.noreply.github.com>
2023-02-12 18:56:26 +0200

    Update and rename page 37_45 my.js to code-samples/page-37_45-my.js
    true or false **&** compare pigs

---
+++ code-samples/page-37_45-my.js
@@ -0,0 +1,22 @@
+//true or false
+let isWeekend = true;
+let tankIsFull = false;
+let PorkMeat = true;
+let chikenMeat = false;
+let Mangal = false;
+let shouldGotoShashlik = isWeekend && tankIsFull && (PorkMeat || chikenMeat) && Mangal;
+
+console.log('Should we go to shashlik? ' + shouldGotoShashlik);
+
+//compare pigs
+var dadWeight = 101;
+var MomWeight = 83.5;
+var MaxWeight = 74;
+
+//cinema
+var permissionAge = 14;
+var adultHere = true;
+var childAge = 12;
+var canWatchFilm = childAge >= permissionAge || adultHere;
+console.log(canWatchFilm);
+
